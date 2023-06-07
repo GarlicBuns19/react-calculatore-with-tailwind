b3f9a5b50d45dbf4ae119e2afc18c69d7cf86644
GarlicBuns19 <planterx26@gmail.com>
2023-06-07 20:56:34 +0200

    Added functionality checking for new operator and end of string

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -3,14 +3,16 @@ import {useCalculator} from "../contexts/Calculator.jsx";
 export function Head(props) {
     const cal = useCalculator();
     const operatorArr = ['+', '-', '*', '/', '.', '%'];
+    var evalStr = cal.evaluationString;
+    var evalStrSecondLastChar = evalStr.at(-2);
 
-    if (operatorArr.includes(cal.evaluationString.at(-2)) && operatorArr.includes(cal.evaluationString.at(-1))) {
-        cal.evaluationString = cal.evaluationString.slice(0, -1)
+    if (operatorArr.includes(evalStrSecondLastChar) && operatorArr.includes(evalStr.at(-1))) {
+        cal.evaluationString = evalStr.replace(evalStrSecondLastChar ,'')
     }
 
     return (
         <div className={"mb-2 p-2 border-2 border-solid rounded-md"}>
-            <div className={"p-5 text-lg text-right bg-zinc-600 rounded-md w-96 overflow-x-auto"}>{cal.evaluationString || cal.result}</div>
+            <div className={"p-5 text-lg text-right bg-zinc-600 rounded-md w-96 overflow-x-auto"}>{evalStr || cal.result}</div>
         </div>
     )
 }
