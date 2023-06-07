8c43631486fc61ec0f72ae292becd18d64f279a8
GarlicBuns19 <planterx26@gmail.com>
2023-06-07 20:56:57 +0200

    Updated CalculatorHistory rendering

--- src/components/CalculatorHistory.jsx
+++ src/components/CalculatorHistory.jsx
@@ -3,10 +3,15 @@ import {useCalculator} from "../contexts/Calculator.jsx";
 export function CalculatorHistory(props) {
     const cal = useCalculator();
 
-    return (
+    if (cal.history.length > 0) return (
         <div>
-            <h2>History</h2>
-            {cal.history.map((item,index) => <div key={index}>{item.cal}</div>)}
+            <h2>Calculation History</h2>
+            {cal.history.map((item, index) => <div key={index}>{item.cal}</div>)}
+        </div>
+    );
+    else return (
+        <div>
+            <h2>No Calculation History</h2>
         </div>
     );
 }
