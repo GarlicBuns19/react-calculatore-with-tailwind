cca387389f71c0f5ae57835ed2bc2397d509b603
GarlicBuns19 <planterx26@gmail.com>
2023-06-09 16:04:40 +0200

    Operator replacement fixed

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -3,16 +3,14 @@ import {useCalculator} from "../contexts/Calculator.jsx";
 export function Head(props) {
     const cal = useCalculator();
     const operatorArr = ['+', '-', '*', '/', '.', '%'];
-    var evalStr = cal.evaluationString;
-    var evalStrSecondLastChar = evalStr.at(-2);
 
-    if (operatorArr.includes(evalStrSecondLastChar) && operatorArr.includes(evalStr.at(-1))) {
-        cal.evaluationString = evalStr.replace(evalStrSecondLastChar ,'')
+    if (operatorArr.includes(cal.evaluationString.at(-2)) && operatorArr.includes(cal.evaluationString.at(-1))) {
+        cal.evaluationString = cal.evaluationString.replace(cal.evaluationString.at(-2), "")
     }
 
     return (
         <div className={"mb-2 p-2 border-2 border-solid rounded-md"}>
-            <div className={"p-5 text-lg text-right bg-zinc-600 rounded-md w-96 overflow-x-auto"}>{evalStr || cal.result}</div>
+            <div className={"p-5 text-lg text-right bg-zinc-600 rounded-md w-96 overflow-x-auto"}>{cal.evaluationString || cal.result}</div>
         </div>
     )
 }
